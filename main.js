b003b331b8925cd33b20d63c29c262cca5bd6cb8
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2023-05-12 13:41:04 +0100

    REFACTOR: Ajustement de la position des fichiers de Polices et de leurs chemins d'accès dans le fichier FONT-FACE. | REFACTOR: Ajustement des chemins d'accès dans les différentes pages de l'application et REFACTOR des commentaires dans les fichiers main.js et preloader.js. | STYLE: Ajout d'une icône à l'application TEAM.

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
-// todo: Initialisation du processus de cycle de vie de l'application
+// TODO: Initialisation du processus de cycle de vie de l'application
 
-// init: Initialisation et appel des modules utiles pour l'application
+// INIT: Initialisation et appel des modules utiles pour l'application
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-// build: Création de la fenêtre et initialisation de son cycle de vie
+// BUILD: Création de la fenêtre et initialisation de son cycle de vie
 const createWindow = () => {
-    // Création et initialisation dela fenêtre de l'application
+    // Création et initialisation de la fenêtre de l'application
     const window = new BrowserWindow({
         width: 1366,
         height: 768,
-        icon: "Logo.ico",
+        icon: 'Logo.ico',
         webPreferences: {
             preload: path.join(__dirname, './preloader.js')
         }
     })
 
-    // Chargement du fichier d'entré de l'application (la première page qui doit s'afficher au démarrage de l'application)
+    // FEAT: Supprimer le menu présent par défaut sur les applications Electron.js
+    // window.setMenu(null)
+    // build: Chargement du fichier d'entré de l'application (la première page qui doit s'afficher au démarrage de l'application)
     window.loadFile('./App/FrontEnd/Public/Pages/Login.html')
 }
 
 
-// build: Initialisation de la promesse d'attente de l'affichage de la fenêtre
+// BUILD: Initialisation de la promesse d'attente de l'affichage de la fenêtre
 app.whenReady().then(() => {
     createWindow()
 
-    // build: garder l'application en exécution lorsque aucune fenêtre n'est plus ouverte
+    // BUILD: garder l'application en exécution lorsque aucune fenêtre n'est plus ouverte
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-// build: Cas de crash de l'application dans le cas où l'application n'a aucune fenêtre ouverte et n'est pas sous MacOs
+// BUILD: Cas de crash de l'application dans le cas où l'application n'a aucune fenêtre ouverte et n'est pas sous MacOs
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
--- preloader.js
+++ preloader.js
@@ -1,4 +1,4 @@
-// todo: Initialisation du processus d'affichage du module process de l'application
+// TODO: Initialisation du processus d'affichage du module process de l'application
 
 // feat: Ajout du processus d'affichage des versions de dépendences
 window.addEventListener('DOMContentLoaded', () => {
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (element) element.innerText = text
     }
 
+    // FEAT: faire une boucle pour permettre d'afficher les versions des différents process parmi la liste présente
     for (const dependency of ['chrome', 'node', 'electron']) {
         replaceText(`${dependency}-version`, process.versions[dependency])
     }
